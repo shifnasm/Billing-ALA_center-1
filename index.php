58659e387ac767129f46f3f84e1d7e76b2101891
Juzly ahamed <2018is003@stu.ucsc.cmb.ac.lk>
2021-04-20 12:52:39 +0530

    login validation

--- index.php
+++ index.php
@@ -1,56 +1,54 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Company Login</title>
-        <link rel="stylesheet" href="style/loginstyle.css">
-       
-    </head>
 
-    <body>
-      <div class="login-form">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Company Login</title>
+    <link rel="stylesheet" href="style/loginstyle.css">
+
+</head>
+
+<body>
+    <div class="login-form">
         <h1>Login</h1>
         <div class="container">
             <div class="main">
                 <div class="content">
                     <img class="logo" src="img/logo1.png">
-                    
-                    <form action="" method="post">
 
-                        <input type="text" name="" placeholder="User Name" required autofocus="">
+                    <form action="loginvalid.php" method="POST">
+                        <?php if (isset($_GET['error'])) { ?>
+                        <p class="error"><?php echo $_GET['error']; ?></p>
+                        <?php } ?>
 
-                         <input type="password" name="" placeholder="Password" required autofocus="">
+                        <input type="text" name="username" placeholder="User Name" required autofocus="">
 
-                         <button class="btn" type="submit">Login</button>
+                        <input type="password" name="password" placeholder="Password" required autofocus="">
+
+                        <button class="btn" type="submit">Login</button>
                     </form>
-                    
+
                 </div>
-                
+
                 <div class="form-img">
                     <img src="img/login.png">
-                    
+
 
                 </div>
 
 
             </div>
-            
+
 
 
         </div>
-          
-
-      </div>
 
 
-    </body>
+    </div>
+
+
+</body>
 
 
 </html>
-
-
-
-
-
-
--- product.php
+++ product.php
@@ -1,3 +1,10 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['AdminUser'])) {
+        header('Location: index.php');
+        die();
+    }
+?>
 <!DOCTYPE html>
 <html>
 
