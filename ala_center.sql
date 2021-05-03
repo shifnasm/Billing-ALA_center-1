33af64395c6f8e088b698e1d207587fd54d511d9
Madhu-26 <madhushadavidmathivannan@gmail.com>
2021-05-03 14:15:39 +0530

    DB update

--- ala_center.sql
+++ ala_center.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 28, 2021 at 03:20 PM
--- Server version: 10.4.18-MariaDB
--- PHP Version: 8.0.3
+-- Generation Time: May 03, 2021 at 10:44 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.3.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -40,7 +41,8 @@ CREATE TABLE `customers` (
 --
 
 INSERT INTO `customers` (`shop_code`, `shop_name`, `owner`, `contact`, `address`) VALUES
-('S0001', 'Shifna', 'Poonakutty', 756895485, 'Colombo 07');
+('S0001', 'Shifna', 'Poonakutty', 756895485, 'Colombo 07'),
+('S0002', 'Breezebe', 'Juz', 778899665, 'Kattankudy');
 
 -- --------------------------------------------------------
 
@@ -51,15 +53,47 @@ INSERT INTO `customers` (`shop_code`, `shop_name`, `owner`, `contact`, `address`
 CREATE TABLE `products` (
   `product_code` varchar(10) NOT NULL,
   `product_name` varchar(40) NOT NULL,
-  `unit_price` int(40) NOT NULL
+  `unit_price` int(40) NOT NULL,
+  `quantity` int(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`product_code`, `product_name`, `unit_price`) VALUES
-('A0001', 'Poonakutty', 1000000);
+INSERT INTO `products` (`product_code`, `product_name`, `unit_price`, `quantity`) VALUES
+('A0001', 'Poonakutty', 1000000, 3),
+('A0002', 'Book', 1000, 10);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchase`
+--
+
+CREATE TABLE `purchase` (
+  `id` int(25) NOT NULL,
+  `date` varchar(255) NOT NULL,
+  `total` int(50) NOT NULL,
+  `pay` int(50) NOT NULL,
+  `due` int(50) NOT NULL,
+  `payment_type` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchase_item`
+--
+
+CREATE TABLE `purchase_item` (
+  `id` int(25) NOT NULL,
+  `purchase_id` int(25) NOT NULL,
+  `product_code` int(25) NOT NULL,
+  `buy_price` int(50) NOT NULL,
+  `quantity` int(50) NOT NULL,
+  `total` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
@@ -76,6 +110,34 @@ ALTER TABLE `customers`
 --
 ALTER TABLE `products`
   ADD PRIMARY KEY (`product_code`);
+
+--
+-- Indexes for table `purchase`
+--
+ALTER TABLE `purchase`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `purchase_item`
+--
+ALTER TABLE `purchase_item`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `purchase`
+--
+ALTER TABLE `purchase`
+  MODIFY `id` int(25) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `purchase_item`
+--
+ALTER TABLE `purchase_item`
+  MODIFY `id` int(25) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
